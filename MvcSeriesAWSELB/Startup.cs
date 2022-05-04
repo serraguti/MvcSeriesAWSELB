6019bbc62c8cfa06a4fbff85d13511d45a15875b
serraguti <pacoserranox@gmail.com>
2022-05-04 14:09:24 +0200

    commit

--- MvcSeriesAWSELB/Startup.cs
+++ MvcSeriesAWSELB/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcSeriesAWSELB.Data;
+using MvcSeriesAWSELB.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +26,11 @@ namespace MvcSeriesAWSELB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string cadena =
+                this.Configuration.GetConnectionString("awsmysql");
+            services.AddTransient<RepositorySeries>();
+            services.AddDbContext<SeriesContext>
+                (options => options.UseMySql(cadena, ServerVersion.AutoDetect(cadena)));
             services.AddControllersWithViews();
         }
 
