3065db9e11a1baec2abb5a92a85e48ee24bcf76c
serraguti <pacoserranox@gmail.com>
2022-05-04 13:59:53 +0200

    commit

--- MvcSeriesAWSELB/Repositories/RepositorySeries.cs
+++ MvcSeriesAWSELB/Repositories/RepositorySeries.cs
@@ -1,4 +1,5 @@
 ﻿using MvcSeriesAWSELB.Data;
+using MvcSeriesAWSELB.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,41 @@ namespace MvcSeriesAWSELB.Repositories
             this.context = context;
         }
 
+        public List<Serie> GetSeries()
+        {
+            return this.context.Series.ToList();
+        }
 
+        public Serie FindSerie(int id)
+        {
+            return this.context.Series.FirstOrDefault(z => z.IdSerie == id);
+        }
+
+        private int GetMaxIdSerie()
+        {
+            if (this.context.Series.Count() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return this.context.Series.Max(x => x.IdSerie) + 1;
+            }
+        }
+
+        public void InsertSerie(string nombre, string imagen
+            , double puntuacion, int anyo)
+        {
+            Serie serie = new Serie
+            {
+                IdSerie = this.GetMaxIdSerie()
+                ,Nombre = nombre
+                ,Imagen = imagen
+                ,Puntuacion = puntuacion
+                ,Anyo = anyo
+            };
+            this.context.Series.Add(serie);
+            this.context.SaveChanges();
+        }
     }
 }
