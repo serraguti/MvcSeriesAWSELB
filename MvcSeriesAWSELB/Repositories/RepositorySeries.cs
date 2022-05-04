dd27531319779ee7eebc59aeeeb3925b28a1e963
serraguti <pacoserranox@gmail.com>
2022-05-04 13:54:44 +0200

    Add project files.

---
+++ MvcSeriesAWSELB/Repositories/RepositorySeries.cs
@@ -0,0 +1,20 @@
+﻿using MvcSeriesAWSELB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSeriesAWSELB.Repositories
+{
+    public class RepositorySeries
+    {
+        private SeriesContext context;
+
+        public RepositorySeries(SeriesContext context)
+        {
+            this.context = context;
+        }
+
+
+    }
+}
